2cc5a69ac19972e7669c6465db8818cff3c9c5a0
whsu <2298536003@qq.com>
2021-03-04 16:24:03 +0800

    modified configuration structure

--- config.cpp
+++ config.cpp
@@ -4,6 +4,54 @@
 
 namespace wbackup {
 
+int __config_t::get_config() {
+    fs::path new_dest_json = destination / "config.json";
+
+    // config.json shall be readable
+    std::fstream json_in(new_dest_json, std::ios::in);
+    if (!json_in) return 1;
+
+    // json is well-formed
+    nlohmann::json j;
+    try {
+        json_in >> j;
+    } catch (std::exception &ex) {
+        return 2;
+    }
+
+    // complusory values
+    //  * source directory path
+    try {
+        std::string new_src_str;
+        nlohmann::from_json(j["source"], new_src_str);
+        source = fs::path(new_src_str);
+    } catch (std::exception &ex) {
+        return 3;
+    }
+
+    // optional values
+    //  * maximum backup size
+    try {
+        nlohmann::from_json(j["maximimSize"], max_size);
+    } catch (std::exception &ex) {
+        max_size = 0x280000000ll;  // 10GB
+    }
+
+    // optional values
+    //  * excluded folders
+    excluded.clear();
+    try {
+        auto excluded_list = j["exclude"];
+        for (auto &i : excluded_list) {
+            std::string s;
+            nlohmann::from_json(i, s);
+            excluded[s] = true;
+        }
+    } catch (std::exception &e) {
+        
+    }
+}
+
 int __config_t::change_destination(char const *new_dest) {
     fs::path path(new_dest);
     return change_destination(new_dest);
@@ -19,20 +67,6 @@ int __config_t::change_destination(const fs::path &new_dest) {
     fs::directory_entry new_dest_json(new_dest/"config.json");
     if (!(new_dest_json.is_regular_file())) return 2;
 
-    // config.json shall be readable
-    std::fstream json_in(new_dest_json.path(), std::ios::in);
-    if (!json_in) return 3;
-
-    // json is well-formed
-    nlohmann::json j;
-    try { 
-        json_in >> j;
-    } catch (std::exception &ex) {
-        return 4;
-    }
-
-    // there is source folder info in json
-    
 }
 
 }
--- headers/config.h
+++ headers/config.h
@@ -10,8 +10,16 @@ namespace wbackup {
 // data structure to store configurations
 class __config_t {
 private:
-    // backup source path and destination path
+    // backup destination path (canonical)
+    // all information in this structure is read from
+    // config.json in this directory
     fs::path destination;
+
+    // denotes whether the destination has correct 
+    // config.json
+    bool available;
+
+    // backup source path (canonical)
     fs::path source;
 
     // files and directories excluded from the backup
@@ -20,13 +28,20 @@ private:
     // maximum size of the backup
     uint64_t max_size;
 
+    // read configuration from the destination selected now
+    // called on change_destination()
+    int get_config();
+
 public:
     __config_t(const __config_t &) = delete;
     __config_t(__config_t &&) = delete;
 
     // Change the backup destination path, as well as the 
-    // source path associated with it, by reading config 
+    //
+    // information associated with it, by reading config 
     // json in the new destination folder.
+    //
+    // checks if new_dest exists and json file exists
     int change_destination(char const *new_dest);
     int change_destination(const fs::path &new_dest);
 
@@ -36,8 +51,11 @@ public:
     int change_source(const fs::path &new_dest);
 
     // source and destination path getter
-    fs::path get_source_path();
-    fs::path get_destination_path();
+    const fs::path &get_source_path() const;
+    const fs::path &get_destination_path() const;
+
+    // check the selected backup is available
+    bool is_available() const;
 
 } configuration;
 
