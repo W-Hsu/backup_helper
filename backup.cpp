4234ecbd395ba9fa10f34a23173b023f6a250494
whsu <whsu@localhost>
2021-03-06 22:53:48 +0800

    Finished full backup algo

--- backup.cpp
+++ backup.cpp
@@ -7,8 +7,25 @@ namespace fs = std::filesystem;
 
 namespace wbackup {
 
-void full_backup() {
-    static fs::path destination_of_this_time = configuration.get_destination_path() / "back_data" / get_system_time();
+void full_backup(fs::path now_relative) {
+    static fs::path dst = configuration.get_destination_path() / "back_data" / get_system_time();
+    const fs::path &src = configuration.get_source_path();
+
+    if (configuration.is_excluded(now_relative))
+        return;
+    
+    fs::directory_iterator di(src/now_relative);
+    for (auto &every_ent: di) {
+        fs::path child_relative = every_ent.path();
+        fs::relative(child_relative, src);
+
+        if (every_ent.is_directory()) {
+            fs::create_directory(dst/child_relative);
+            full_backup(child_relative);
+        }
+        else if (every_ent.is_regular_file())
+            fs::copy_file(src/child_relative, dst/child_relative);  // TODO exception handling
+    } 
 }
 
 void merge_backup() {
--- misc.h
+++ misc.h
@@ -6,9 +6,11 @@
 #include <sstream>
 #include <chrono>
 #include <ctime>
+#include <filesystem>
 #include <boost/format.hpp>
 
 namespace cron = std::chrono;
+namespace fs = std::filesystem;
 using fmt = boost::format;
 
 namespace wbackup {
@@ -26,13 +28,25 @@ void split(const std::string &s, std::vector<std::string> &splitted, char delim 
 
 std::string get_system_time() {
     time_t now_timestamp = cron::system_clock::to_time_t(cron::system_clock::now());
-    struct tm *ptm = gmtime(now_timestamp);
+    struct tm *ptm = gmtime(&now_timestamp);
 
     return (
         //    y    m    d    h    m    s
         fmt("%d_%02d_%02d_%02d_%02d_%02d")
             % ptm->tm_year % ptm->tm_mon % ptm->tm_mday % ptm->tm_hour % ptm->tm_min % ptm->tm_sec
     ).str();
+}
+
+bool is_same(const fs::path &a, const fs::path &b) {
+    fs::directory_entry a_ent(a), b_ent(b);
+
+    if (a_ent.is_regular_file() && b_ent.is_regular_file()) {
+        return
+            (a_ent.last_write_time()==b_ent.last_write_time()) &&
+            (a_ent.file_size()==b_ent.file_size());
+    }
+    else
+        return false;
 }
 
 }  // namespace wbackup
